3febad9fc7ebf7becd7dee72bbe4ab4748fa4d6b
burakcakir <burakckr@windowslive.com>
2023-07-14 22:02:59 +0300

    - register api integration complete

--- BidHubTestt/lib/register.dart
+++ BidHubTestt/lib/register.dart
@@ -1,8 +1,9 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:url_launcher/url_launcher.dart';
-
+import 'package:http/http.dart' as http;
 import 'register.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -112,7 +113,7 @@ class _RegisterPageState extends State<RegisterPage> {
               ),
               SizedBox(height: 20.0),
               ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
                   // Kayıt işlemlerini gerçekleştir
                   String username = usernameController.text;
                   String name = nameController.text;
@@ -124,9 +125,75 @@ class _RegisterPageState extends State<RegisterPage> {
                   String address = addressController.text;
 
                   // Kayıt işlemleri burada yapılabilir
+                  // REST API endpointini ve gönderilecek verileri ayarlayın
+                  String url = 'https://bidhubappprod.azurewebsites.net/api/User/AddUserAsync'; // API'nizin URL'sini buraya yazın
+                  Map<String, String> headers = {'Content-Type': 'application/json'};
+                  Map<String, String> jsonBody = {
+                    'userName': username,
+                    'name': name,
+                    'surname': surname,
+                    'password': password,
+                    'phoneNumber': phoneNumber,
+                    'identityNumber': identityNumber,
+                    'address': address,
+                    'email': email
+                  };
 
+                  try
+                  {
+                    http.Response response = await http.post(
+                      Uri.parse(url),
+                      headers: headers,
+                      body: jsonEncode(jsonBody),
+                    );
+
+                    if (response.statusCode == 200) {
+                      // Giriş işlemi başarılı olduğunda anasayfaya yönlendir
+                      Navigator.pushReplacementNamed(context, '/home');
+                    } else {
+                      // İstek başarılı olmadıysa veya kullanıcı girişi hatalıysa hata mesajını gösterin
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Hata'),
+                            content: Text('Kayıt yapılamadı. Lütfen tekrar deneyin.'),
+                            actions: [
+                              TextButton(
+                                child: Text('Tamam'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  }
+                  catch(e)
+                  {
+                  // İstek gönderilirken bir hata oluştuysa hata mesajını gösterin
+                  showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                  return AlertDialog(
+                  title: Text('Hata'),
+                  content: Text('Bir hata oluştu. Lütfen tekrar deneyin.'),
+                  actions: [
+                  TextButton(
+                  child: Text('Tamam'),
+                  onPressed: () {
+                  Navigator.of(context).pop();
+                  },
+                  ),
+                  ],
+                  );
+                  },
+                  );
+                  }
                   // Kayıt işlemi başarılı olduğunda anasayfaya yönlendir
-                  Navigator.pushReplacementNamed(context, '/home');
+
                 },
                 child: Text('Kayıt Ol'),
               ),
